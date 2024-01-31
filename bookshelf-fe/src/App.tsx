a4e9085d89fdc57b2d41f0cdf00b5161efa1614a
tafifa <tafifa09@gmail.com>
2024-01-31 19:42:18 +0700

    clear main branch

--- bookshelf-fe/src/App.tsx
+++ bookshelf-fe/src/App.tsx
@@ -1,17 +1,11 @@
-import Header from './components/header/header';
-import Footer from './components/footer/footer';
-import MainPage from './components/main/mainPage';
-import ScrollUp from './components/additional/scrollUp';
+import MainPage from './components/mainPage';
 
 import './App.css';
 
 export default function App() {
   return (
     <>
-      <Header />
-      <MainPage />
-      <ScrollUp />
-      <Footer />
+      <MainPage/>
     </>
   )
 }
--- bookshelf-fe/src/components/footer/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="p-5 bg-cyan-950 text-white pb-10">
-      <h2 className="mb-14">
-        by <em>Tafif Assiddiqi</em><span>&#10024;</span> @ <a href="https://tafifa.github.io">tafifa.github.io</a>
-      </h2>
-    </footer>
-  )
-}
