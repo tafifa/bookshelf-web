35f7514ccabe76190fdd2615bba122d58fca7592
tafifa <tafifa09@gmail.com>
2024-02-17 14:37:50 +0700

    rework pagebar (unfinished)

--- bookshelf-fe/src/App.jsx
+++ bookshelf-fe/src/App.jsx
@@ -1,15 +1,25 @@
+import { useState } from 'react';
+
 import Header from './components/header/header';
-import PageBar from './components/pageBar/pageBar';
+import PathBar from './components/utils/Pathbar';
+import SideBar from './components/utils/Sidebar';
 import MainPage from './components/main/mainPage';
 import Footer from './components/footer/footer';
 
-import './App.css';
+import './styles/App.css';
 
 export default function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  
+  const handleToggleDrawer = () => {
+    setIsOpen((prevState) => !prevState)
+  }
+
   return (
     <>
       <Header />
-      <PageBar />
+      <PathBar  />
+      <SideBar open={isOpen} onClose={handleToggleDrawer} />
       <MainPage/>
       <Footer />
     </>
