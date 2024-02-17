bd7385346a58aef4eb95649c2f46f7c38e804e7e
nana72848e <d1041211005@student.untan.ac.id>
2024-02-17 14:29:59 +0700

    feat: add dependencies and implement feature search (with google api, but got 403 forbidden when fetching the data (the change is in bookshelf.jsx, toolKit.jsx, and searchBar.jsx)

--- bookshelf-fe/src/components/main/bookshelf/toolKit/searchBar.jsx
+++ bookshelf-fe/src/components/main/bookshelf/toolKit/searchBar.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import { MdSearch } from "react-icons/md";
+import axios from 'axios'
 
-export default function SearchBar() {
+export default function SearchBar({ onSearch }) {
+  const apikey = 'yourapikey'
+  const [query, setQuery] = useState('')
+  const handleSearch = async () => {
+    try{
+      const response = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apikey}`
+      )
+      onSearch(response.data.items)
+    }catch (error){
+      console.error('error fetching data')
+    }
+  }
   return (
     <div className='bg-white border-custom-grey-light border rounded-xl flex items-center '>
-        <input className='px-48 outline-none text-center text-base font-semibold text-custom-grey-light ' type="text" placeholder='Search your book here...'/>
-        <MdSearch className='w-[26px] h-[26px] mr-2 fill-custom-grey-light ' />
+        <input className='px-48 outline-none text-center text-base font-semibold text-custom-grey-light ' type="text" placeholder='Search your book here...' value={query} onChange={(e) => setQuery(e.target.value)} />
+        <button>
+        <MdSearch onClick={handleSearch} className='w-[26px] h-[26px] mr-2 fill-custom-grey-light cursor-pointer ' />
+        </button>
     </div>
   )
 }
--- bookshelf-fe/src/components/main/bookshelf/toolKit/toolKit.jsx
+++ bookshelf-fe/src/components/main/bookshelf/toolKit/toolKit.jsx
@@ -2,11 +2,14 @@ import SearchBar from './searchBar'
 import EditBtn from './editBtn'
 import AddBtn from './addBtn'
 
-export default function ToolKit() {
+export default function ToolKit({ onSearchResults }) {
+  const handleSearchResults = (results) => {
+    onSearchResults(results)
+  }
   return (
     <div className='flex justify-between pt-10 px-10'>
       <h2 className='text-left font-semibold text-3xl text-custom-grey-bold '>List Book</h2>
-      <SearchBar />
+      <SearchBar onSearch={handleSearchResults} />
       <div className='flex gap-2'>
         <EditBtn />
         <AddBtn />
